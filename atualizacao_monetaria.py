6b1e0ce7bbe5a449d149d9fd5d17e55b4f48ad6a
GabrielDully <gabrieldully@usp.br>
2024-03-02 19:04:40 -0300

    Upgrade: atualizacao_monetaria.py
    Funções -> Classe
    As funções adicionadas na versão anterior foram substituídas por métodos correspondentes dentro da classe 'atualizacao_monetaria'.
     - consultar (new)
     - carregar_df (new)
     - download_pdf (old)
     - gerar_df (old)
    
    Strings -> Variáveis de Classe
    Algumas strings que estavam sendo utilizadas na estrutura de definição das funções foram substituídas por variáveis de classe que podem ser alteradas pelo desenvolvedor de modo simples e direto, i. e., modificando o conteúdo de:
     - url
     - pkl_path
     - pdf_path
    
    DownloadError()
    Em caso de erro no download, o Python levanta um erro definido por esta classe, cuja classe pai é Exception.

--- atualizacao_monetaria.py
+++ atualizacao_monetaria.py
@@ -1,46 +1,94 @@
 import pandas as pd
 import requests
 import tabula
+from pathlib import Path
 import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 
-url = "https://www.tjsp.jus.br/Download/Tabelas/TabelaDebitosJudiciais.pdf"
+class DownloadError(Exception):
+    pass
 
-def download_pdf():
-    resposta = requests.get(url)
+class atualizacao_monetaria(object):
 
-    if resposta.status_code == 200:
-        with open('TabelaDebitosJudiciais.pdf', 'wb') as arquivo_pdf:
-            arquivo_pdf.write(resposta.content)
-        print('Download concluído com sucesso.')
-    else:    
-        print(f"{Fore.RED}Falha ao baixar arquivo em: {Fore.RESET}'{url}'")
+    url = "https://www.tjsp.jus.br/Download/Tabelas/TabelaDebitosJudiciais.pdf"
+    pkl_path = 'indices_INPC.pkl'
+    pdf_path = 'TabelaDebitosJudiciais.pdf'
 
-def gerar_df():
+    def __init__(self):
+        self.df = atualizacao_monetaria.carregar_df() 
 
-    def validar_coluna(nome_coluna):
-        caracteres = nome_coluna.split()
-
-        if all(caractere.isdigit() for caractere in caracteres):
-            return (True, ''.join(caracteres))
-        else:
-            return (False,)
-
-    try:
-        tabelas = tabula.read_pdf('TabelaDebitosJudiciais.pdf', pages='all')
-
-        df = pd.DataFrame()
-        for tabela in tabelas:
-            for nome_coluna in tabela.columns:
-                tupla = validar_coluna(nome_coluna)
-                if tupla[0] == True:
-                    df[tupla[1]] = tabela[nome_coluna]
+    def consultar(self, data: str):
+        try:
+            # Espera-se o padrão DD/MM/YYYY.
+            componentes = data.split('/')
+            indice = self.df[componentes[2]][int(componentes[1]) - 1]
+            indice = indice.replace('.', '')
+            indice = indice.replace(',', '.')
+            return float(indice)
         
-        print(df)
+        except IndexError:
+            raise IndexError(f"{Fore.RED}Formato de data não correspondente.{Fore.RESET}\nFormato fornecido: '{data}'\nFormato esperado: 'DD/MM/YYYY'")
+        
+        except KeyError:
+            raise KeyError(f"{Fore.RED}Ano ou mês inválidos.")
 
-    except FileNotFoundError:
-        print(f"{Fore.RED}Falha ao carregar arquivo: {Fore.RESET}'TabelaDebitosJudiciais.pdf'")
 
-download_pdf()
-gerar_df()
+    @classmethod
+    def carregar_df(cls):
+        try:
+            return pd.read_pickle(atualizacao_monetaria.pkl_path)
+        
+        except FileNotFoundError:
+            if not Path(atualizacao_monetaria.pdf_path).exists():
+                atualizacao_monetaria.download_pdf()
+            atualizacao_monetaria.gerar_df()
+            return atualizacao_monetaria.carregar_df()
+
+
+    @classmethod
+    def download_pdf(cls):
+        resposta = requests.get(cls.url)
+        if resposta.status_code == 200:
+            with open(cls.pdf_path, 'wb') as arquivo_pdf:
+                arquivo_pdf.write(resposta.content)
+        else:    
+            raise DownloadError(f"Falha no download de: '{cls.url}'") 
+
+
+    @classmethod
+    def gerar_df(cls):
+
+        def validar_coluna(nome_coluna):
+            caracteres = nome_coluna.split()
+            if all(caractere.isdigit() for caractere in caracteres):
+                return (True, ''.join(caracteres))
+            else:
+                return (False,)
+
+        try:
+            tabelas = tabula.read_pdf(cls.pdf_path, pages='all')
+            df = pd.DataFrame()
+            for tabela in tabelas:
+                for nome_coluna in tabela.columns:
+                    tupla = validar_coluna(nome_coluna)
+                    if tupla[0] == True:
+                        df[tupla[1]] = tabela[nome_coluna]
+            df.to_pickle(cls.pkl_path)
+
+        except FileNotFoundError:
+            cls.download_pdf()
+
+
+# Exemplos de uso do módulo.
+x = atualizacao_monetaria().consultar('11/12/1964')
+print(x)
+
+# Exemplo de uso do módulo.
+y = atualizacao_monetaria()
+
+indice1 = y.consultar('03/02/2024')
+print(indice1)
+
+indice2 = y.consultar('09/03/1998')
+print(indice2)
